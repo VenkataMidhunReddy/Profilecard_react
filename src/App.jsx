c0c38665602d127b7fe3feca21f5b80b4c9a3468
Venkata Midhun Reddy <abhinavmidhun32@gmail.com>
2025-08-22 13:43:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Profilecard from './components/Profilecard'
+
+function App() {
+  return (
+    <>
+      <Profilecard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Profilecard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Profilecard.css';
+import Profileimage from '../assets/profile.jpg';
+
+const Profilecard = () => {
+  return (
+    <div className="upc">
+      <div className="gradient"></div>
+      <div className="profile-down">
+        <img src={Profileimage} alt="Profile" className="profile-img" />
+      </div>
+      <div className="profile-title">
+        Gurrala Venkata Midhun Reddy</div>
+      <div className="profile-description">
+        <h3>Web Developer</h3>
+      </div>
+      <div className="profile-button">
+        <a href="mailto:elon@example.com" className="profile-contact">Contact Me</a>
+      </div>
+      <div className='skill'>
+        <span>HTML</span>
+        <span>JavaScript</span>
+        <span>CSS</span>
+        <span>React</span>
+
+      </div>
+    </div>
+  );
+};
+
+export default Profilecard;
